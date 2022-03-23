0e262b5f21f31b2825b6bb6fb517c6b76206caad
Mrfroxi <froxi340@mail.ru>
2022-03-23 19:38:36 +0300

    removed bug of  connecting to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,38 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './modules/user/user.module'; 
+import { UsersModule } from './modules/user/user.module';
 import { User } from './modules/user/entity/user.enity';
+
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'root',
-    database: 'pani',
-    entities: [User],
-    synchronize: true,
-  }), UsersModule],
+
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: 5432,
+      username: "postgres",
+      password: "root",
+      database: process.env.POSTGRES_DB,
+      entities: [User],
+      synchronize: true,
+    }), 
+
+    UsersModule,
+
+    ConfigModule.forRoot({
+      envFilePath:  ['.env.development.local', '.env.development'],
+      isGlobal: true
+    })
+
+  ],
+
   controllers: [AppController],
+
   providers: [AppService],
+
 })
+
 export class AppModule { }
