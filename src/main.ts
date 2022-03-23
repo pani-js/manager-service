f8c774486df9306a3b395d0798f2e1fb6f96a476
Mrfroxi <froxi340@mail.ru>
2022-03-23 15:57:57 +0300

    added swagger to the project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Paim example')
+    .setDescription('The Pain API description')
+    .setVersion('1.0')
+    .addTag('Pain')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
