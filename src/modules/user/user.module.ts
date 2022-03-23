dda6373f900160371efcd0ea74030a82d6486be9
Mrfroxi <froxi340@mail.ru>
2022-03-23 20:06:55 +0300

    improved name of file

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,13 +1,13 @@
 
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { UsersService } from './user.service';
-// import { UsersController } from './user.controller';
+import { UsersService } from './user.service';
+import { UsersController } from './user.controller';
 import { User } from './entity/user.enity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [],
-  controllers: [],
+  providers: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule { }
