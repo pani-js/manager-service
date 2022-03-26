d182e28eb514e8b54a35756f6c0040ab73519511
Mrfroxi <froxi340@mail.ru>
2022-03-26 12:12:06 +0300

    refactor:add type to function addSwaggerConfig

--- src/swaggerConfig.ts
+++ src/swaggerConfig.ts
@@ -1,15 +1,16 @@
+import { INestApplication } from '@nestjs/common/interfaces/nest-application.interface';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-function addSwaggerConfig(args) {
+function addSwaggerConfig(app: INestApplication): void {
   const config = new DocumentBuilder()
     .setTitle('Pani example')
     .setDescription('The Pain API description')
     .setVersion('0.0.1')
     .build();
 
-  const document = SwaggerModule.createDocument(args, config);
+  const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('swagger', args, document);
+  SwaggerModule.setup('swagger', app, document);
 }
 
 export { addSwaggerConfig };
